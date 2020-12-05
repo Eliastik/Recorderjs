7373acda360a21014800e3846cfd4e8047ca0358
Eliastik <26941242+Eliastik@users.noreply.github.com>
2020-12-05 16:19:19 +0100

    Allowing changing the audio node recorded

--- src/recorder.js
+++ src/recorder.js
@@ -16,26 +16,7 @@ export class Recorder {
 
     constructor(source, cfg) {
         Object.assign(this.config, cfg);
-        this.context = source.context;
-        this.node = (this.context.createScriptProcessor ||
-        this.context.createJavaScriptNode).call(this.context,
-            this.config.bufferLen, this.config.numChannels, this.config.numChannels);
-
-        this.node.onaudioprocess = (e) => {
-            if (!this.recording) return;
-
-            var buffer = [];
-            for (var channel = 0; channel < this.config.numChannels; channel++) {
-                buffer.push(e.inputBuffer.getChannelData(channel));
-            }
-            this.worker.postMessage({
-                command: 'record',
-                buffer: buffer
-            });
-        };
-
-        source.connect(this.node);
-        this.node.connect(this.context.destination);    //this should not be necessary
+        this.setup(source);
 
         let self = {};
         this.worker = new InlineWorker(function () {
@@ -203,6 +184,33 @@ export class Recorder {
                 cb(e.data.data);
             }
         };
+    }
+
+    setup(source) {
+        if(this.node) { // Disconnect previous node
+            this.node.disconnect();
+        }
+
+        this.context = source.context;
+        this.node = (this.context.createScriptProcessor ||
+        this.context.createJavaScriptNode).call(this.context,
+            this.config.bufferLen, this.config.numChannels, this.config.numChannels);
+
+        this.node.onaudioprocess = (e) => {
+            if (!this.recording) return;
+
+            var buffer = [];
+            for (var channel = 0; channel < this.config.numChannels; channel++) {
+                buffer.push(e.inputBuffer.getChannelData(channel));
+            }
+            this.worker.postMessage({
+                command: 'record',
+                buffer: buffer
+            });
+        };
+
+        source.connect(this.node);
+        this.node.connect(this.context.destination);    //this should not be necessary
     }
 
 
